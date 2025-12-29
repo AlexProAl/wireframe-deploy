28d209f68d2de0a9bb6490ea5831ca866937c732
Alex <pronin@globaldrive.ru>
2025-12-29 03:46:48 +0300

    fix: fix

--- src/components/price/price.tsx
+++ src/components/price/price.tsx
@@ -15,38 +15,40 @@ export default function PricingSection() {
         </div>
 
         {/* Cards */}
-        <div className="grid gap-8 md:grid-cols-2">
-          <PricingCard
-            title="Тариф лайт"
-            description="Подходит для небольших команд и старта"
-            price="3000₽"
-            period="/ в месяц"
-            features={[
-              'До 10ти аккаутов',
-              'Базовые возможности настройки',
-              'Основные инструменты аналитики',
-              'Поддержка по электронной почте',
-            ]}
-            buttonText="Лайт тариф"
-            cardClassName="bg-white text-secondary"
-            buttonClassName="bg-tertiary text-white hover:opacity-90"
-          />
+        <div className="mx-auto max-w-full xl:max-w-[900px]">
+          <div className="grid gap-8 md:grid-cols-2">
+            <PricingCard
+              title="Тариф лайт"
+              description="Подходит для небольших команд и старта"
+              price="3000₽"
+              period="/ в месяц"
+              features={[
+                'До 10ти аккаутов',
+                'Базовые возможности настройки',
+                'Основные инструменты аналитики',
+                'Поддержка по электронной почте',
+              ]}
+              buttonText="Лайт тариф"
+              cardClassName="bg-white text-secondary"
+              buttonClassName="bg-tertiary text-white hover:opacity-90"
+            />
 
-          <PricingCard
-            title="Тариф Про"
-            description="Для команд, которым нужен максимум возможностей"
-            price="9000₽"
-            period="/ в месяц"
-            features={[
-              'До 50ти аккаунтов',
-              'Полные возможности настроек',
-              'Продвинутая система аналитики',
-              'Приоритетная поддержка по электронной почте',
-            ]}
-            buttonText="Перейти на про"
-            cardClassName="bg-tertiary text-white"
-            buttonClassName="bg-white text-tertiary hover:bg-gray-100"
-          />
+            <PricingCard
+              title="Тариф Про"
+              description="Для команд, которым нужен максимум возможностей"
+              price="9000₽"
+              period="/ в месяц"
+              features={[
+                'До 50ти аккаунтов',
+                'Полные возможности настроек',
+                'Продвинутая система аналитики',
+                'Приоритетная поддержка по электронной почте',
+              ]}
+              buttonText="Перейти на про"
+              cardClassName="bg-tertiary text-white"
+              buttonClassName="bg-white text-tertiary hover:bg-gray-100"
+            />
+          </div>
         </div>
       </div>
     </section>
